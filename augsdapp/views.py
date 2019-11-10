8b3ab9d74cc4f6f85a62f3ccb5852a1ff53cce14
2018A7PS0091G <f20180091@goa.bits-pilani.ac.in>
2019-11-11 03:07:35 +0530

    course model created

--- augsdapp/views.py
+++ augsdapp/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def homepage(request):
     return render(request, 'augsdapp/homepage.html', {})
+
+def AddCourse(request):
+    return render(request, 'augsdapp/AddCourse.html', {})
+
+def ModifyCourse(request):
+    return render(request, 'augsdapp/ModifyCourse.html', {})
+
+def DeleteCourse(request):
+    return render(request, 'augsdapp/DeleteCourse.html', {})
