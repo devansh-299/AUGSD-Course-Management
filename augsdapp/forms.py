45c0cdf2374ddde60442a81055ac95f01daa90ef
2018A7PS0091G <f20180091@goa.bits-pilani.ac.in>
2019-11-16 23:16:24 +0530

    mm

--- augsdapp/forms.py
+++ augsdapp/forms.py
@@ -1,11 +1,17 @@
 from django import forms
 from .models import Course,SecClass
+# from bootstrap_datepicker_plus import DatePickerInput
 
 class AddCourseForm(forms.ModelForm):
 
 	class Meta:
 		model = Course
 		fields = ('courseCode','courseName','courseIC','midsemDateTime','compreDateTime')
+		# widgets = {
+        #     'midsemDateTime': DatePickerInput(), # default date-format %m/%d/%Y will be used
+        #     'compreDateTime': DatePickerInput(format='%Y-%m-%d'), # specify date-frmat
+        # }
+
 
 
 class AddSectionForm(forms.ModelForm):
